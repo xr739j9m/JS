3cd78aea6eed69c23037ae0cd1a77f8c091c2157
tenshio <av956797@gmail.com>
2022-07-09 11:56:05 +0800

    random

---
+++ test.js
@@ -0,0 +1,13 @@
+console.log('hello shit') ;
+
+var num_1=2
+var num_2=3
+
+console.log(num_1+num_2)
+
+let total = 0, count = 1
+while (count <= 10) {
+total += count;
+count += 1;
+}
+console.log(total);

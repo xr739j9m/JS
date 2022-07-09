7bb0d71b696d76bd38147fc7554672d30ed6a06f
tenshio <av956797@gmail.com>
2022-07-09 13:30:36 +0800

    chapter 1

---
+++ chapter1.js
@@ -0,0 +1,26 @@
+console.log('hello shit') ;
+
+var num_1=2
+var num_2=3
+
+console.log(num_1+num_2)
+
+let total = 0, count = 1
+while (count <= 10) {
+total += count;
+count += 1;
+}
+console.log(total);
+
+console.log('lessgoo')
+console.log((1-2)+1)
+
+//Special numbers:
+//1. Infinity, -Infinity
+//2. Infinity - 1
+console.log(0/0)
+//-> NaN -- not a number , doesnt yield meaningful result
+
+console.log(`yo\nsup`) //newline
+console.log(`yo\tsup`) //tab
+console.log("con" + "cat" + "e" + "nate")
